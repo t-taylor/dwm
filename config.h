f7c94a819a52461c77224413fe24b074c0a93c45
Tom Taylor <ttaylor3.142@gmail.com>
2021-02-24 16:02:34 +0000

    Add push patch

--- config.h
+++ config.h
@@ -76,6 +76,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,         togglebar,      {0} },
 	{ MODKEY,                       XK_j,         focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,         focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,         pushdown,       {0} },
+	{ MODKEY|ShiftMask,             XK_k,         pushup,         {0} },
 	{ MODKEY,                       XK_a,         incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_x,         incnmaster,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_h,         setmfact,       {.f = -0.05} },
