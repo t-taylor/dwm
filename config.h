3799319ce1596db13473fc329531ba3162526e7b
t-taylor <ttaylor3.142@gmail.com>
2023-07-01 17:17:52 +0100

    Reduce bar font

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=16" };
+static const char *fonts[]          = { "monospace:size=10" };
 static const char col_bg[]          = "#282828";
 static const char col_gray1[]        = "#928374";
 static const char col_gray2[]        = "#a89984";
